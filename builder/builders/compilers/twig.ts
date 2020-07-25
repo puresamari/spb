ab3d4ec50d95f95f7099fab1d09668379295fddd
Simon Rothert <puresamari@gmail.com>
2020-07-25 23:18:03 +0200

    added path to twig compiler for extends and includes

--- builder/builders/compilers/twig.ts
+++ builder/builders/compilers/twig.ts
@@ -1,17 +1,28 @@
 import * as fs from 'fs';
+import path from 'path';
 import { twig } from 'twig';
 
 import { ExportType } from '../utils';
 import { IBuilderContext } from './../../utils';
 
-const path = require('path');
+const pretty = require('pretty');
 
 export async function CompileTWIG(file: string, exportPath: string, context: IBuilderContext) {
   return new Promise<{ path: string, type: ExportType }>(resolve => {
     const data = fs.readFileSync(file, "utf8");
-    const template = twig({ data }).render({ spb: context });
-    fs.writeFileSync(exportPath, template, { });
-    resolve({ path: exportPath, type: 'html' });
+    try {
+      const template = twig({
+        data,
+        allowInlineIncludes: true,
+        async: false,
+        path: path.dirname(file) + '/'
+      } as any);
+      fs.writeFileSync(exportPath, pretty(template.render({ spb: context })), { });
+      resolve({ path: exportPath, type: 'html' });
+    } catch(e) {
+      console.log('Error while compiling twig', e)
+      resolve()
+    }
   });
   
 }

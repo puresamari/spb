c71625861affcc5745814a6e716412d5af31c630
Simon Rothert <puresamari@gmail.com>
2020-07-31 17:00:34 +0200

    fixed 'test'

--- builder/builders/compilers/compiler.ts
+++ builder/builders/compilers/compiler.ts
@@ -2,9 +2,7 @@ import { IBuilderContext } from './../../definitions';
 import { ExportType } from './../utils';
 
 export abstract class Compiler {
-  constructor() {
-    console.log('constructing the compiler');
-  }
+  constructor() { }
 
   public abstract async compile(file: string, exportPath: string, context: IBuilderContext):
     Promise<{ path: string, type: ExportType, affectedFiles: string[] }>;

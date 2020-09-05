52fca586b704b937ed7785a635749508efb523da
Simon Rothert <puresamari@gmail.com>
2020-09-06 00:08:22 +0200

    more percise schema definitions

--- builder/definitions/builder-options.ts
+++ builder/definitions/builder-options.ts
@@ -1,10 +1,25 @@
 export interface IBuilderOptions {
   output: string;
+
+  /**
+   * Files that should be built.
+   */
   files: string[];
   compilers?: {
     postcss?: {
       plugins?: string[]
     }
   },
+
+  /**
+   * A script that will be ran after the build has finished.
+   */
   postBuild?: string;
+  
+  /**
+   * Determains whether the output folder should be cleared.
+   *
+   * @default true
+   */
+  clearOutputFolder?: boolean; // default is true
 }

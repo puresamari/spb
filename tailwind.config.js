bf524e4b7f022eb5cfb14507b359345867ae9499
Simon Rothert <puresamari@gmail.com>
2020-09-12 22:35:09 +0200

    added initial page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: { },
+    extend: {
+      colors: {
+        blue: {
+          400: '#415FDF',
+          600: '#3551C7'
+        }
+      },
+      borderRadius: {
+        default: '2rem',
+        md: '4rem',
+        lg: '8rem'
+      }
+    },
     fontFamily: {
       sans: ['"Monofur"', 'sans-serif']
     }

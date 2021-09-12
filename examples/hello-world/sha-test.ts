c8ad2a2dc32f8e25b101b7869f623839c9c11dbe
Simon Rothert <puresamari@gmail.com>
2021-09-12 18:25:55 +0200

    added more complex sha module for testing purposes

--- examples/hello-world/sha-test.ts
+++ examples/hello-world/sha-test.ts
@@ -3,14 +3,11 @@
 // Option 2: Import just the parts you need.
 // import * as THREE from 'three';
 // import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
-import * as asdf from "fast-sha256";
-
+import hash from "hash.js";
 
 export default function init() {
-  console.log(asdf);
-  const d = new Uint8Array([21,31]);
- 
-  const h = new asdf.HMAC(d); // also Hash and HMAC classes
-  const mac = h.update(d).digest();
-
+  console.log(hash.sha256().update('abc').digest('hex'));
+  console.log(hash.sha256().update('abc').digest('hex'));
+  console.log(hash.sha256().update('abc').digest('hex'));
+  console.log(hash.sha256().update('abc').digest('hex'));
 }
